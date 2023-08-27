3b18abaab1b30fd358fdbf52446f57e120c595db
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-08-27 23:13:04 +0400

    wip: add display rounds section

---
+++ src/Display/Display.tsx
@@ -0,0 +1,27 @@
+import Cups from "../assets/cups-balls.png"
+import Lockup from "../assets/lockup.png"
+import Smiley from "../assets/smiley.png"
+import Blossom from "../assets/blossom.png"
+import Switches from "../assets/switches.png"
+import Box from "../assets/boxception.png"
+import Cloud from "../assets/cloud.png"
+import Site from "../assets/site-point.png"
+
+
+function Pilot() {
+  return (
+    <>
+    <p>Battle #4 - Display</p>
+    <img src={Cups}/>
+    <img src={Lockup}/>
+    <img src={Smiley}/>
+    <img src={Blossom}/>
+    <img src={Switches}/>
+    <img src={Box}/>
+    <img src={Cloud}/>
+    <img src={Site}/>
+    </>
+  )
+}
+
+export default Pilot
