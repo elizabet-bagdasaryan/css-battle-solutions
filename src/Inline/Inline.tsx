3e9ce2a0e0db3aadaa03741283f7969433d29c46
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-08-28 12:09:26 +0400

    wip:add two sections

---
+++ src/Inline/Inline.tsx
@@ -0,0 +1,17 @@
+import Suffocate from "../assets/suffocate.png"
+import Horizon from "../assets/horizon.png"
+
+
+
+function Inline() {
+
+  return (
+    <>
+    <p>Battle #5 - Inline</p>
+    <img src={Suffocate}/>
+    <img src={Horizon}/>
+    </>
+  )
+}
+
+export default Inline
