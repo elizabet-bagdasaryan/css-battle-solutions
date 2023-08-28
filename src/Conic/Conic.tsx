5f707b8cac0ce39f19aea79ba39a6eae2ce6c0f7
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-08-28 12:09:13 +0400

    wip: add two sections

---
+++ src/Conic/Conic.tsx
@@ -0,0 +1,17 @@
+import Equals from "../assets/equals.png"
+import Bandaid from "../assets/bandaid.png"
+
+
+
+function Conic() {
+
+  return (
+    <>
+    <p>Battle #5 - Inline</p>
+    <img src={Equals}/>
+    <img src={Bandaid}/>
+    </>
+  )
+}
+
+export default Conic
